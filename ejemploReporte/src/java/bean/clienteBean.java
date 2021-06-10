bf2c1ce5501227b3feb7c0625ab5ded64c94ce40
segarod <srodriguezlab@yahoo.com>
2021-06-10 00:28:47 -0300

    Video 2

---
+++ ejemploReporte/src/java/bean/clienteBean.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
+@ManagedBean
+@ViewScoped
+public class clienteBean implements Serializable{
+
+    public clienteBean() {
+    }
+    //Metodo para invocar el reporte y enviarle los parametros si es que necesita  
+    public void verReporte() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        
+        //Instancia hacia la clase reporteClientes        
+        reporteCliente rCliente = new reporteCliente();
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+        String ruta = servletContext.getRealPath("/reportes/reporteCliente.jasper");
+       
+        rCliente.getReporte(ruta);        
+        FacesContext.getCurrentInstance().responseComplete();               
+    }
+}
